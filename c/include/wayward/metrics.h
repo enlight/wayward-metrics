1a69f9f47fee1007a2c5bb850f543b35f1943066
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-01-23 00:27:06 +0700

    Add the missing headers.

--- c/include/wayward/metrics.h
+++ c/include/wayward/metrics.h
@@ -1,8 +1,10 @@
 #ifndef _WAYWARD_METRICS_H_
 #define _WAYWARD_METRICS_H_
 
+#include "wayward/metrics/buffer.h"
 #include "wayward/metrics/connection.h"
 #include "wayward/metrics/frame.h"
+#include "wayward/metrics/socket.h"
 #include "wayward/metrics/version.h"
 
 #endif
