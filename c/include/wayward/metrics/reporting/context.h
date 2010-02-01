e98269be1f473572eb6fec79dc2057b9c6abcae9
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-02-01 13:30:32 +0700

    Support recording data about execution contexts for basic profiling data.

---
+++ c/include/wayward/metrics/reporting/context.h
@@ -0,0 +1,12 @@
+#ifndef _WAYWARD_METRICS_REPORTING_CONTEXT_H_
+#define _WAYWARD_METRICS_REPORTING_CONTEXT_H_
+
+#include "wayward/metrics/reporting/base.h"
+
+extern void wwm_reporter_context_create(wwm_reporter_t reporter, uint64_t context_id, const char *name);
+extern void wwm_reporter_context_enter(wwm_reporter_t reporter, uint64_t context_id);
+extern void wwm_reporter_context_enter_by_name(wwm_reporter_t reporter, const char *name);
+extern void wwm_reporter_context_exit(wwm_reporter_t reporter);
+
+#endif
+
---
+++ c/src/reporting/context.c
@@ -0,0 +1,60 @@
+#include "wayward/metrics/reporting/context.h"
+#include "wayward/metrics/reporting/constants.h"
+#include "wayward/metrics/bert_encode.h"
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+wwm_reporter_context_create(wwm_reporter_t reporter, uint64_t context_id, const char *name)
+{
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_bert_push_begin_tuple(data, 2);
+    data = wwm_bert_push_int32(data, METRICS_EVENT_CONTEXT_CREATE);
+    data = wwm_bert_push_uint64(data, context_id);
+    data = wwm_bert_push_string(data, name);
+    wwm_reporter_record_data(reporter, data);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+wwm_reporter_context_enter(wwm_reporter_t reporter, uint64_t context_id)
+{
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_bert_push_begin_tuple(data, 2);
+    data = wwm_bert_push_int32(data, METRICS_EVENT_CONTEXT_ENTER);
+    data = wwm_bert_push_uint64(data, context_id);
+    wwm_reporter_record_data(reporter, data);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+wwm_reporter_context_enter_by_name(wwm_reporter_t reporter, const char *name)
+{
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_bert_push_begin_tuple(data, 2);
+    data = wwm_bert_push_int32(data, METRICS_EVENT_CONTEXT_ENTER_BY_NAME);
+    data = wwm_bert_push_string(data, name);
+    wwm_reporter_record_data(reporter, data);
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+void
+wwm_reporter_context_exit(wwm_reporter_t reporter)
+{
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_bert_push_begin_tuple(data, 1);
+    data = wwm_bert_push_int32(data, METRICS_EVENT_CONTEXT_EXIT);
+    wwm_reporter_record_data(reporter, data);
+}
+
