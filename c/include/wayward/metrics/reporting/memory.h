a973e036dccca53e6f6d7a8a00072b34c0d2912a
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-02-01 22:13:15 +0700

    Flesh this out some more.

--- c/include/wayward/metrics/reporting/memory.h
+++ c/include/wayward/metrics/reporting/memory.h
@@ -3,15 +3,15 @@
 
 #include "wayward/metrics/reporting/reporter.h"
 
-extern void wwm_reporter_memory_heap_new(wwm_reporter_t reporter, int heap_id, const char* heap_name);
-extern void wwm_reporter_memory_heap_destroy(wwm_reporter_t reporter, int heap_id);
+extern void wwm_reporter_memory_heap_new(wwm_reporter_t reporter, int32_t heap_id, const char* heap_name);
+extern void wwm_reporter_memory_heap_destroy(wwm_reporter_t reporter, int32_t heap_id);
 
-extern void wwm_reporter_memory_malloc(wwm_reporter_t reporter, int heap_id, void* addr, size_t);
-extern void wwm_reporter_memory_realloc(wwm_reporter_t reporter, int heap_id, void* old_addr, void* new_addr, size_t new_size);
-extern void wwm_reporter_memory_free(wwm_reporter_t reporter, int heap_id, void* addr);
+extern void wwm_reporter_memory_malloc(wwm_reporter_t reporter, int32_t heap_id, void* addr, size_t);
+extern void wwm_reporter_memory_realloc(wwm_reporter_t reporter, int32_t heap_id, void* old_addr, void* new_addr, size_t new_size);
+extern void wwm_reporter_memory_free(wwm_reporter_t reporter, int32_t heap_id, void* addr);
 
-extern void wwm_reporter_memory_gc_start(wwm_reporter_t reporter, int heap_id);
-extern void wwm_reporter_memory_gc_end(wwm_reporter_t reporter, int heap_id);
+extern void wwm_reporter_memory_gc_start(wwm_reporter_t reporter, int32_t heap_id);
+extern void wwm_reporter_memory_gc_end(wwm_reporter_t reporter, int32_t heap_id);
     
 #endif
 
--- c/src/reporting/memory.c
+++ c/src/reporting/memory.c
@@ -6,7 +6,7 @@
 /**
 */
 void
-wwm_reporter_memory_heap_new(wwm_reporter_t reporter, int heap_id, const char* heap_name)
+wwm_reporter_memory_heap_new(wwm_reporter_t reporter, int32_t heap_id, const char* heap_name)
 {
     wwm_buffer_t data = wwm_buffer_new(512);
     data = wwm_reporter_populate_base_record_data(reporter, data);
@@ -21,7 +21,7 @@ wwm_reporter_memory_heap_new(wwm_reporter_t reporter, int heap_id, const char* h
 /**
 */
 void
-wwm_reporter_memory_heap_destroy(wwm_reporter_t reporter, int heap_id)
+wwm_reporter_memory_heap_destroy(wwm_reporter_t reporter, int32_t heap_id)
 {
     wwm_buffer_t data = wwm_buffer_new(512);
     data = wwm_reporter_populate_base_record_data(reporter, data);
@@ -35,31 +35,55 @@ wwm_reporter_memory_heap_destroy(wwm_reporter_t reporter, int heap_id)
 /**
 */
 void
-wwm_reporter_memory_malloc(wwm_reporter_t reporter, int heap_id, void* addr, size_t size)
+wwm_reporter_memory_malloc(wwm_reporter_t reporter, int32_t heap_id, void* addr, size_t size)
 {
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_codec_push_begin_tuple(data, 4);
+    data = wwm_codec_push_int32(data, METRICS_EVENT_MEMORY_MALLOC);
+    data = wwm_codec_push_int32(data, heap_id);
+    data = wwm_codec_push_uint64(data, (uint64_t)(uintptr_t)addr);
+    data = wwm_codec_push_uint64(data, size);
+    wwm_reporter_record_data(reporter, data);
 }
 
 //------------------------------------------------------------------------------
 /**
 */
 void
-wwm_reporter_memory_realloc(wwm_reporter_t reporter, int heap_id, void* old_addr, void* new_addr, size_t new_size)
+wwm_reporter_memory_realloc(wwm_reporter_t reporter, int32_t heap_id, void* old_addr, void* new_addr, size_t new_size)
 {
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_codec_push_begin_tuple(data, 5);
+    data = wwm_codec_push_int32(data, METRICS_EVENT_MEMORY_REALLOC);
+    data = wwm_codec_push_int32(data, heap_id);
+    data = wwm_codec_push_uint64(data, (uint64_t)(uintptr_t)old_addr);
+    data = wwm_codec_push_uint64(data, (uint64_t)(uintptr_t)new_addr);
+    data = wwm_codec_push_uint64(data, new_size);
+    wwm_reporter_record_data(reporter, data);
 }
 
 //------------------------------------------------------------------------------
 /**
 */
 void
-wwm_reporter_memory_free(wwm_reporter_t reporter, int heap_id, void* addr)
+wwm_reporter_memory_free(wwm_reporter_t reporter, int32_t heap_id, void* addr)
 {
+    wwm_buffer_t data = wwm_buffer_new(512);
+    data = wwm_reporter_populate_base_record_data(reporter, data);
+    data = wwm_codec_push_begin_tuple(data, 3);
+    data = wwm_codec_push_int32(data, METRICS_EVENT_MEMORY_FREE);
+    data = wwm_codec_push_int32(data, heap_id);
+    data = wwm_codec_push_uint64(data, (uint64_t)(uintptr_t)addr);
+    wwm_reporter_record_data(reporter, data);
 }
 
 //------------------------------------------------------------------------------
 /**
 */
 void
-wwm_reporter_memory_gc_start(wwm_reporter_t reporter, int heap_id)
+wwm_reporter_memory_gc_start(wwm_reporter_t reporter, int32_t heap_id)
 {
     wwm_buffer_t data = wwm_buffer_new(512);
     data = wwm_reporter_populate_base_record_data(reporter, data);
@@ -73,7 +97,7 @@ wwm_reporter_memory_gc_start(wwm_reporter_t reporter, int heap_id)
 /**
 */
 void
-wwm_reporter_memory_gc_end(wwm_reporter_t reporter, int heap_id)
+wwm_reporter_memory_gc_end(wwm_reporter_t reporter, int32_t heap_id)
 {
     wwm_buffer_t data = wwm_buffer_new(512);
     data = wwm_reporter_populate_base_record_data(reporter, data);
