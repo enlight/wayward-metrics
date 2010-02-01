efc94fbb25a8cf17627da30f16c89df1e95482fe
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-02-01 16:49:31 +0700

    Renumber data types, add new ones for uint64_t and timestamps.

--- c/src/codec.c
+++ c/src/codec.c
@@ -4,13 +4,15 @@
 
 #define WWM_CODEC_PROTOCOL_VERSION      131
 
-#define WWM_CODEC_SMALL_INTEGER_TYPE     97
-#define WWM_CODEC_INTEGER_TYPE           98
-#define WWM_CODEC_ATOM_TYPE             100
-#define WWM_CODEC_SMALL_TUPLE_TYPE      104
-#define WWM_CODEC_LARGE_TUPLE_TYPE      105
-#define WWM_CODEC_STRING_TYPE           107
-#define WWM_CODEC_BINARY_TYPE           109
+#define WWM_CODEC_SMALL_INTEGER_TYPE    200 
+#define WWM_CODEC_INTEGER_TYPE          201
+#define WWM_CODEC_UINT64_TYPE           202
+#define WWM_CODEC_ATOM_TYPE             204
+#define WWM_CODEC_SMALL_TUPLE_TYPE      205
+#define WWM_CODEC_LARGE_TUPLE_TYPE      206
+#define WWM_CODEC_STRING_TYPE           207
+#define WWM_CODEC_BINARY_TYPE           208
+#define WWM_CODEC_TIMESTAMP_TYPE        209
 
 //------------------------------------------------------------------------------
 /**
@@ -70,9 +72,16 @@ wwm_codec_push_int32(wwm_buffer_t buf, int32_t value)
 wwm_buffer_t
 wwm_codec_push_uint64(wwm_buffer_t buf, uint64_t value)
 {
+    uint32_t top, bottom;
+    uint64_t mask = 0xffffffff;
+
+    top = (value >> 32) & mask;
+    bottom = value & mask;
+
     buf = wwm_buffer_ensure(buf, 5);
-    buf = wwm_buffer_append_int8(buf, WWM_CODEC_INTEGER_TYPE);
-    buf = wwm_buffer_append_int32(buf, value);
+    buf = wwm_buffer_append_int8(buf, WWM_CODEC_UINT64_TYPE);
+    buf = wwm_buffer_append_int32(buf, top);
+    buf = wwm_buffer_append_int32(buf, bottom);
     return buf;
 }
 
@@ -116,9 +125,7 @@ wwm_codec_push_timestamp(wwm_buffer_t buf, const struct timeval *tv)
     unsigned int seconds      = tv->tv_sec % 1000000;
     unsigned int microseconds = tv->tv_usec;
 
-    buf = wwm_codec_push_begin_tuple(buf, 5);
-    buf = wwm_codec_push_atom(buf, "atom");
-    buf = wwm_codec_push_atom(buf, "time");
+    buf = wwm_codec_push_int32(buf, WWM_CODEC_TIMESTAMP_TYPE);
     buf = wwm_codec_push_int32(buf, megaseconds);
     buf = wwm_codec_push_int32(buf, seconds);
     buf = wwm_codec_push_int32(buf, microseconds);
