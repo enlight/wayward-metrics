7739fa04983b6f22bc13705d350bfb6862bf719c
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-02-01 12:02:12 +0700

    Get rid of a debug printf.

--- c/src/posix/thread.c
+++ c/src/posix/thread.c
@@ -11,7 +11,6 @@ uint64_t wwm_thread_get_current_id(void)
 {
     union { pthread_t thread_val; uint64_t uint64_val; } caster = { };
     caster.thread_val = pthread_self();
-    printf("ID: %lld\n", caster.uint64_val);
     return caster.uint64_val;
 }
 
